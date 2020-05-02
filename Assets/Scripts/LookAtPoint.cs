c827eaf4cc84515daca06bcf63fb2c1939efb5c1
Marcello Tridenti <marcello.tridenti@googlemail.com>
2020-05-02 21:23:50 +0200

    Initial prototype

---
+++ Assets/Scripts/LookAtPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace TicToc.Input
+{
+    public class LookAtPoint : MonoBehaviour
+    {
+        GameObject sphere;
+
+        private void Start()
+        {
+            sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+
+        private void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50, 1 << 8))
+            {
+                sphere.transform.position = new Vector3(hit.point.x, hit.point.y, transform.position.z);
+                transform.rotation = Quaternion.LookRotation(new Vector3(hit.point.x, hit.point.y, transform.position.z) - transform.position, Vector3.up);
+            }
+        }
+    }
+}
