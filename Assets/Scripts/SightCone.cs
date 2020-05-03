d31d10902d50d7d82cb2659e67992c4e0b268472
Marcello Tridenti <marcello.tridenti@googlemail.com>
2020-05-03 16:20:47 +0200

    Working prototype

--- Assets/Scripts/SightCone.cs
+++ Assets/Scripts/SightCone.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using TicToc.Mechanics;
 using UnityEngine;
 
 public class SightCone : MonoBehaviour
@@ -10,29 +12,72 @@ public class SightCone : MonoBehaviour
     private float sightAngle = 30;
 
     [SerializeField]
-    private int numberOfRays = 30;
+    private int sections = 30;
 
-    private List<Ray> rays;
+    private List<Ray> rays = new List<Ray>();
     private float rayAngle;
+
+    private List<SightState> objectsInSight = new List<SightState>();
 
     private void Start()
     {
-        rayAngle = sightAngle / numberOfRays;
+        rayAngle = sightAngle / sections;
     }
 
     private void Update()
     {
-        rays = new List<Ray>();
+        rays.Clear();
         float startingAngle = -sightAngle / 2;
-        for(int i = 0; i <= numberOfRays; ++i)
+        for(int i = 0; i <= sections; ++i)
         {
             Vector3 rayDirection = Quaternion.AngleAxis(startingAngle + i * rayAngle, transform.right) * transform.forward;
             rays.Add(new Ray(transform.position, rayDirection));
-            Debug.DrawRay(transform.position, rayDirection * sightRange, Color.yellow, Time.deltaTime);
+            //Debug.DrawRay(transform.position, rayDirection * sightRange, Color.yellow, Time.deltaTime);
+        }
+
+        RaycastHit obstacleHit;
+        RaycastHit[] hits;
+
+        List<SightState> hitObjects = new List<SightState>();
+        foreach (Ray ray in rays)
+        {
+            float distance = sightRange;
+
+            if (Physics.Raycast(ray, out obstacleHit, sightRange, 1 << 9))
+            {
+                distance = obstacleHit.distance;
+                //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, Time.deltaTime);
+            }
+            else
+            {
+                //Debug.DrawRay(ray.origin, ray.direction * sightRange, Color.yellow, Time.deltaTime);
+            }
+
+            hits = Physics.RaycastAll(ray, distance);
+
+            Debug.DrawRay(ray.origin, ray.direction * distance, Color.yellow, Time.deltaTime);
+
+            foreach (RaycastHit hit in hits)
+            {
+                SightState sightState = hit.collider.gameObject.GetComponent<SightState>();
+                if (sightState != null)
+                {
+                    sightState.SetSightState(true);
+                    hitObjects.Add(sightState);
+                }
+            }
+
+            IEnumerable<SightState> lostSight = objectsInSight.Where(sightState => hitObjects.Contains(sightState) == false);
+            foreach(SightState sightState in lostSight)
+            {
+                sightState.SetSightState(false);
+            }
+
+            objectsInSight = hitObjects;
         }
 
         Debug.DrawRay(transform.position, transform.forward * sightRange, Color.cyan, Time.deltaTime);
-        Debug.DrawRay(transform.position, Quaternion.AngleAxis(sightAngle / 2, transform.right) * transform.forward * sightRange, Color.red, Time.deltaTime);
-        Debug.DrawRay(transform.position, Quaternion.AngleAxis(-sightAngle / 2, transform.right) * transform.forward * sightRange, Color.red, Time.deltaTime);
+        //Debug.DrawRay(transform.position, Quaternion.AngleAxis(sightAngle / 2, transform.right) * transform.forward * sightRange, Color.red, Time.deltaTime);
+        //Debug.DrawRay(transform.position, Quaternion.AngleAxis(-sightAngle / 2, transform.right) * transform.forward * sightRange, Color.red, Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/SightState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace TicToc.Mechanics
+{
+    public class SightState : MonoBehaviour
+    {
+        private bool inSight;
+
+        public event EventHandler<SightStateEventArgs> SightStateChanged;
+
+        public void SetSightState(bool inSight)
+        {
+            if(this.inSight == inSight)
+            {
+                return;
+            }
+
+            this.inSight = inSight;
+            SightStateChanged?.Invoke(this, new SightStateEventArgs(inSight));
+        }
+    }
+}
